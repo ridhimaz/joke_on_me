2fe5f71c209d527a2c6e0c8eff7e72d8d72a17d4
Saskham Negi <negisaksham363@gmail.com>
2024-11-27 22:23:18 +0530

    Added api wrapper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
+import { fetchJokes } from './services/api-service';
+import { makepromt } from './services/utils';
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const prompt = makepromt("Robin", "Software Developer", "extrovert", "12A.M");
+        const result = await fetchJokes(prompt);
+        setData(result);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      
+      {data}
     </div>
   );
 }
