035e4a4bd6262ca4e539af18d6bc973cd65482ba
ridhimaz <ridhimakohli19@gmail.com>
2024-12-01 23:46:43 +0530

    added content

--- src/Components/HomePageGrid.js
+++ src/Components/HomePageGrid.js
@@ -10,21 +10,31 @@ import { Link } from 'react-router-dom';
 function HomePageGrid() {
   return (
     <Grid2 container spacing={10} className='homePageGridContainer'>
-
       <Grid2 className="homePageGrid" size={{ xs: 12, md: 6, lg: 6 }}>
         <Box>
-          <Link to="/jokes">
-            <p id="homePageGridSwipeHeading">Laugh! Laugh! Laugh!</p>
-          </Link>
+          <p id="homePageGridSwipeHeading">Click to laugh</p>
           <div className='homePageGridFire'>
             <img src={fire} id="homePageFire"></img>
             <p className="HomePageGridSwipePara">Error 404: Ego Not Found, </p>
           </div>
           <div className='homePageGridBtnDiv'>
             <p className="">To get ROFL - Rolling On Floor Laughing, </p>
-            <Button className="homePageGridBtn"variant="outlined">Click me!</Button>
+            <Link to="/jokes">
+              <Button
+                sx={{
+                  color: 'white',
+                  width: '100%',
+                  border: '1px solid white',
+                  '&:hover': {
+                    backgroundColor: 'transparent',
+                  },
+                }}
+                variant="outlined"
+              >
+                Click me!
+              </Button>
+            </Link>
           </div>
-
         </Box>
       </Grid2>
       <Grid2 className="homePageGrid" size={{ xs: 12, md: 6, lg: 6 }}>
@@ -36,18 +46,15 @@ function HomePageGrid() {
         </div>
         <div className='homePageGridBox even' size={12} >
           <img className="homePageGridBoxImg" src={woman_laugh}></img>
-          <p className='homePageGridBoxPara'>Software Engineer</p>
-          <p className='homePageGridBoxPara'>"Why did Ridhima prefer coding over socializing?
-            Because the only 'bugs'" </p>
+          <p className='homePageGridBoxPara'>Project Manager</p>
+          <p className='homePageGridBoxPara'>"He’s so punctual, even his tea knows the schedule—2:01 PM sharp, or it's 'project delay' drama."</p>
         </div>
         <div className='homePageGridBox odd2' size={12} >
           <img className="homePageGridBoxImg" src={men_laugh_2}></img>
-          <p className='homePageGridBoxPara'>Software Engineer</p>
-          <p className='homePageGridBoxPara'>"Why did Ridhima prefer coding over socializing?
-            Because the only 'bugs'" </p>
+          <p className='homePageGridBoxPara'>HR</p>
+          <p className='homePageGridBoxPara'>"She types birthday emails with more passion than she’s ever approved a raise—each one a Shakespearean masterpiece."</p>
         </div>
       </Grid2>
-
     </Grid2>
   );
 }
--- src/Components/ResponsiveAppBar.js
+++ src/Components/ResponsiveAppBar.js
@@ -1,42 +1,27 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import "../Components/ResponsiveAppBarCss.css";
+import "../Components/ResponsiveAppBar.css";
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
-const pages = ['Home', 'Contact'];
+const pages = [];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
   return (
     <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none' }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <InsertEmoticonIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-
-          {/* <img src={logo} className='logo' style={{display:"none"}}></img> */}
+          <InsertEmoticonIcon sx={{ display: { md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
+              display: {  md: 'flex' },
               fontFamily: 'monospace',
               fontWeight: 200,
               letterSpacing: '.3rem',
@@ -46,70 +31,6 @@ function ResponsiveAppBar() {
           >
             JokesOnYou
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <InsertEmoticonIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 200,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            JokesOnYou
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
         </Toolbar>
       </Container>
     </AppBar>
